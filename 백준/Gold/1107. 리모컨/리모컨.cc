0950fd0ee334e97a1518a80e437d5cabfd235354
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-01-02 21:15:53 +0900

    [Gold V] Title: 리모컨, Time: 608 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -5,6 +5,7 @@ using namespace std;
 
 int tar;
 string target;
+int si;
 int n;
 int minn = 999999;
 
@@ -12,13 +13,14 @@ vector<int> cap; // 누를 수 있는 버튼들
 vector<int> list;
 
 void search(string s) { // 재귀를 이용한 완전탐색
-	int size = ((int)(s.size()) - (int)(target.size()));
-	
-	if (s!="" && (stoi(s) > 1000000 || size > 1  || s.size()>6 || (s[0]=='0'&&s.size()>1) )) return;
-	if ((s!= "") && s.size() + abs(stoi(s) - tar) < minn) {
-		minn = s.size() + abs(stoi(s) - tar);
+	int size = ((int)(s.size()) - si);
+	if (s != "") {
+		int ss = stoi(s);
+		if ((ss > 1000000 || size > 1 || s.size() > 6 || (s[0] == '0' && s.size() > 1))) return;
+		if (s.size() + abs(stoi(s) - tar) < minn) {
+			minn = s.size() + abs(ss - tar);
+		}
 	}
-	//cout << s << endl;
 	for (int i = 0; i < cap.size(); ++i) {
 		string tmp = s;
 		tmp.append(to_string(cap[i]));
@@ -30,12 +32,12 @@ void search(string s) { // 재귀를 이용한 완전탐색
 int main() {
 	cin >> tar;
 	target = to_string(tar);
+	si = target.size();
 	cin >> n;
 	for (int i = 0; i < n; ++i) {
 		int a; cin >> a;
 		list.push_back(a);
 	}
-	int pointer = 0;
 	for (int i = 0; i < 10; ++i) {
 		bool flag = true;
 		for (int j = 0; j < list.size(); ++j) {
