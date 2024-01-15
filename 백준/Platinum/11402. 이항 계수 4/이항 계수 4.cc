997d3c805aed9d42fbc006c57d1e1896e539441c
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-01-15 18:56:39 +0900

    [Platinum V] Title: 이항 계수 4, Time: 28 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Platinum/11402. 이항 계수 4/이항 계수 4.cc
+++ 백준/Platinum/11402. 이항 계수 4/이항 계수 4.cc
@@ -1,86 +1,27 @@
+#include<iostream>
 
-#include <iostream>
-        
-        
-          
-          #include <vector>
-        
-        
-          
-          using namespace std;
-        
-        
-          
-          #define ll long long
-        
-        
-          
-          #define MAXN 2010
-        
-        
-          
-          int binomial[MAXN][MAXN];
-        
-        
-          
-          ll N, K, M;
-        
-        
-          
-          int main(){
-        
-        
-          
-              cin >> N >> K >> M;
-        
-        
-          
-              for(int i=0; i<M; i++){
-        
-        
-          
-                  binomial[i][0] = 1;
-        
-        
-          
-                  for(int j=1; j<=i; j++)
-        
-        
-          
-                      binomial[i][j] = (binomial[i-1][j-1] + binomial[i-1][j])%M;
-        
-        
-          
-              }
-        
-        
-          
-              int ret = 1;
-        
-        
-          
-              while(N || K){
-        
-        
-          
-                  ret *= binomial[N%M][K%M];
-        
-        
-          
-                  N /= M, K /= M;
-        
-        
-          
-                  ret %= M;
-        
-        
-          
-              }
-        
-        
-          
-              cout << ret;
-        
-        
-          
-          }
+using namespace std;
+
+long long n;
+long long k, m;
+int b[2001][2001];
+
+int main() {
+	cin >> n >> k >> m;
+
+	for (int i = 0; i < m; ++i) { //n
+		b[i][0] = 1;
+		for (int j = 1; j <= i; ++j) { //k
+			b[i][j] = (b[i - 1][j - 1] + b[i - 1][j]) % m; // 포함하거나 안하거나
+		}
+	}
+	int ret = 1;
+
+	while (n || k) {
+		ret *= b[n % m][k % m];
+		n /= m;
+		k /= m;
+		ret %= m;
+	}
+	cout << ret;
+}
