3889c9143ef622ab7c48dbef4c2d5e6acaa4c38d
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-01-17 18:50:05 +0900

    [Gold III] Title: 소수의 연속합, Time: 28 ms, Memory: 15520 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -9,7 +9,7 @@ long long n;
 
 void era() {
 	int cut = 0;
-	for (long long i = 2; i*i <= 4000001; ++i) {
+	for (long long i = 2; i*i < 4000001; ++i) {
 		if (!check[i]) {
 			prime.push_back(i);
 			if (dp.size() > 0) {
@@ -34,16 +34,15 @@ void era() {
 
 int main() {
 	scanf("%d", &n);
-	
+	if (n == 1) {
+		printf("0");
+		return 0;
+	}
 	era(); // 에라토스테네스 체로 소수 만들기
 	
 	int ans = 0;
 
 	if (n < 4) { // 2,3,4이 들어오면 경우의 수가 없으므로 0 리턴
-		if (n == 1) {
-			printf("0");
-			return 0;
-		}
 		printf("1");
 		return 0;
 	}
