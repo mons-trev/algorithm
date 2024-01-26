0c1239b992d35f32b08ba7d3a13c7c9ec2d57751
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-01-26 21:20:35 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string s = "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+    cout << s;
+}
