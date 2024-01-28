4ece6fd3dd4d4e28314656efe27dfc4e830fafd0
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-01-29 00:05:58 +0900

    [Silver IV] Title: 상근이의 여행, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int t;
+int main() {
+    cin >> t;
+    while(t--) {
+        int n,m;
+        cin >> n >> m;
+        for(int i=0; i<m; ++i) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << n-1 << "\n";
+    }
+}
