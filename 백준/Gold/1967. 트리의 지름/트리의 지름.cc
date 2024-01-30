a35b465879efe0b85e9846261d68a615340a9d6c
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-01-30 17:42:08 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2780 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,16 +1,12 @@
-#include<iostream>
+#include<stdio.h>
 #include<vector>
 #include<algorithm>
 
 using namespace std;
 
-// 말단 노드와 말단 노드 사이의 거리이어야한다
-// 그러려면 두 노드 사이의 루트는 반드시 존재해야한다
-
 struct Node {
 	short label;
 	vector<pair<Node*, short>> child; // 자식 노드, 가중치
-	short parent = 0;
 };
 
 Node* tree[10001];
@@ -21,7 +17,7 @@ int findmax(Node* root) {
 		return 0;
 	}
 	vector<int> dist;
-	for (int i = 0; i < root->child.size(); ++i) {
+	for (short i = 0; i < root->child.size(); ++i) {
 		dist.push_back(root->child[i].second + findmax(root->child[i].first));
 	}
 
@@ -35,24 +31,24 @@ int findmax(Node* root) {
 }
 
 int main() {
-	int n;
-	cin >> n;
+	short n;
+	scanf("%d", &n);
 	//트리 생성
-	for (int i = 0; i < 10001; ++i) {
+	for (short i = 0; i < 10001; ++i) {
 		tree[i] = new Node();
 		tree[i]->label = i;
 	}
 	tree[1] = new Node();
 	tree[1]->label = 1;
-	for (int i = 0; i < n-1; ++i) {
+	for (short i = 0; i < n-1; ++i) {
 		short p, c, d;//부모 라벨, 자식 라벨, 가중치
-		cin >> p >> c >> d;
+		scanf("%d %d %d", &p, &c, &d);
 		tree[p]->child.push_back({ tree[c],d });
 		
 	}
 
 	findmax(tree[1]);
 
-	cout << ans;
+	printf("%d", ans);
 
 }
