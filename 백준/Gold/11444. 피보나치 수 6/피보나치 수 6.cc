a1eaabd303fb65dee73df8142583cbda4186621b
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-02-03 18:57:32 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+long long n;
+map <long long, long long> m;
+
+long long fibo(long long a) {
+	if (m[a]) {
+		return m[a];
+	}
+	else {
+		if (a % 2 == 0) {
+			return m[a] = ((fibo(a / 2 + 2) * fibo(a / 2)) + (fibo((a / 2) +1) * fibo((a / 2)-1))) % 1000000007;
+		}
+		else {
+			return m[a] = ((fibo((a+3) / 2) * fibo((a+1) / 2)) + (fibo((a+1) / 2) * fibo((a-1) / 2))) % 1000000007;
+		}
+	}
+}
+
+int main() {
+	cin >> n;
+	m[1] = 0;
+	m[2] = 1;
+	m[3] = 1;
+	m[4] = 2;
+	m[5] = 3;
+	m[6] = 5;
+	
+	cout << fibo(n+1);
+}
