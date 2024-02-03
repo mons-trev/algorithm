bda5299af619d1a339d8a42f059a73e9e62984e4
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-02-04 04:27:00 +0900

    [Gold IV] Title: N-Queen, Time: 1656 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<unordered_set>
+
+using namespace std;
+
+int n;
+
+bool vcol[15] = { false, };
+bool vrd[30] = { false, };
+bool vld[60] = { false, };
+int ans = 0;
+
+void bt(int idx) {
+	if (idx >= n) {
+		ans++;
+		return;
+	}
+	for (int i = 0; i < n; ++i) {
+		if (vcol[i] || vld[(idx-i)+15] || vrd[idx+i]) continue;
+		else {
+			vcol[i] = true;
+			vld[idx - i + 15] = true;
+			vrd[idx + i]=true;
+			bt(idx + 1);
+			vcol[i] = false;
+			vld[idx - i+15] = false;
+			vrd[idx + i] = false;
+		}
+	}
+	return;
+}
+
+int main() {
+	cin >> n;
+	bt(0);
+	cout << ans;
+}
