2eb9251c0aa2e9c7ec07cf413c6530163ebcf43e
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-02-13 18:34:38 +0900

    [Platinum V] Title: 히스토그램, Time: 12 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+#include<stack>
+#include<cmath>
+using namespace std;
+
+vector<long long> list;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	list.push_back(0);
+
+	for (int i = 1; i <= n; ++i) {
+		long long tmp; cin >> tmp;
+		list.push_back(tmp);
+	}
+
+	list.push_back(0);
+	stack<int> s; // ids 저장
+
+	s.push(0);
+
+	long long ans = 0;
+
+	for (int i = 1; i <= n+1; ++i) {
+		if (s.empty() || list[s.top()] <= list[i]) {
+			s.push(i);
+		}
+		else {
+			while (true) {
+				if (s.empty() || list[s.top()] <= list[i]) {
+					break;
+				}
+				else {
+					long long idx = s.top();
+					long long h = list[s.top()];
+					s.pop();
+					ans = max(ans, (i-1-s.top()) * h);
+				}
+			}
+			s.push(i);
+		}
+	}
+	
+	cout << ans;
+}
