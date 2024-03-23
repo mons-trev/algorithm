6d1712216cd33661d801a872cc1aa7dff253f5cc
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-03-23 23:10:18 +0900

    [Gold III] Title: 내리막 길, Time: 104 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,41 +1,52 @@
 #include<iostream>
 #include<vector>
-#include<cmath>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include <iomanip>
+
 using namespace std;
 
-int y, x;
-vector<vector<int>> arr; // 높이 그래프
-vector<vector<int>> dp; // dp
-int dy[4] = { 1,0,0,-1 };
-int dx[4] = { 0,1,-1,0 };
-//dfs와 dp를 같이 이용해야한다.
-int dfsdp(int fy, int fx) {
-	if (fy == y-1 && fx == x-1) {
+
+int dy[4] = {0,1,0,-1};
+int dx[4] = {1,0,-1,0};
+int g[500][500];
+int dp[500][500];
+int n, m;
+
+int dfs(int y, int x) {
+	if (y == n - 1 && x == m - 1) {
+		dp[y][x] = 1;
 		return 1;
 	}
-	if (dp[fy][fx] != -1) {
-		return dp[fy][fx];
-	}
-	dp[fy][fx] = 0;
+
 	for (int i = 0; i < 4; ++i) {
-		int go_y = fy + dy[i];
-		int go_x = fx + dx[i];
-		if (go_y > -1 && go_y<y && go_x > -1 && go_x < x && arr[fy][fx] > arr[go_y][go_x]) {//조건에 맞는지 확인
-			dp[fy][fx] += dfsdp(go_y,go_x);
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny >= 0 && nx >= 0 && ny < n && nx < m) {
+			if (g[ny][nx] < g[y][x]) {
+				if (dp[ny][nx] == -1) {
+					dp[ny][nx] = 0;
+					dp[y][x] += dfs(ny, nx);
+				}
+				else {
+					dp[y][x] += dp[ny][nx];
+				}
+				
+			}
 		}
 	}
-	return dp[fy][fx];
+	return dp[y][x];
 }
+
 int main() {
-	cin >> y >> x;
-	for (int i = 0; i < y; ++i) {
-		vector<int> tmp;
-		dp.push_back({});
-		for (int j = 0; j < x; ++j) {
-			int a; cin >> a; tmp.push_back(a);
-			dp[i].push_back(-1); // dp는 다 0 으로 초기화
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cin >> g[i][j];
+			dp[i][j] = -1;
 		}
-		arr.push_back(tmp);
 	}
-	cout << dfsdp(0, 0);
+	dp[0][0] = 0;
+	cout << dfs(0, 0);
 }
