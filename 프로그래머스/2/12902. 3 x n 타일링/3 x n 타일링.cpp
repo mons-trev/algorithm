7847626ae2caff4d2040b9b487ac065a0cf686c8
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-03-23 21:47:54 +0900

    [level 2] Title: 3 x n 타일링, Time: 9.50 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -2,21 +2,31 @@
 #include <vector>
 #include <iostream>
 #define mod 1000000007
+
 using namespace std;
 
-long long dp[5001];
+long long dp[5001] = {0,};
+
 int solution(int n) {
-    if(n%2==1) return 0;
     dp[2]=3;
-    dp[4]= 2+dp[2]*3; // 11
-    long long cur=2+2*dp[2];
-    for(int i=6; i<=n; i++) {
-        if(i%2!=0) continue;
-        dp[i]=(cur+3*dp[i-2])%mod;
-        cur+=(2*dp[i-2])%mod;
-        cur%=mod;
-        //cout << dp[i] << endl;
+    dp[4]=11;
+    if(n%2!=0) return 0;
+    for(int i=6; i<=n; ++i) {
+        if(i%2==0) {
+            for(int j=2; j<i; ++j) {
+                if(j%2!=0) continue;
+                if(j==i-2) {
+                    dp[i]+=3*(dp[j])%mod;
+                    dp[i]%=mod;
+                }
+                else {
+                    dp[i]+=2*(dp[j])%mod;
+                    dp[i]%=mod;
+                }
+            }
+            dp[i]+=2;
+            dp[i]%=mod;
+        }
     }
-    
-    return dp[n];
+    return dp[n]%mod;
 }
