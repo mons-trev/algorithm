c5c85ef582a3de79f5e3c509030336042ba3095a
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-03-23 17:56:02 +0900

    [Gold IV] Title: 스도쿠, Time: 316 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int g[9][9];
+vector<pair<int, int>> zeros;
+
+void draw() {
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			cout << g[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+bool check(int a, int y, int x) {
+	for (int i = 3 * (y / 3); i < 3 * (y / 3) + 3; ++i) {
+		for (int j = 3 * (x / 3); j < 3 * (x / 3) + 3; ++j) {
+			if (a == g[i][j]) {
+				return false;
+			}
+		}
+	}
+	for (int i = 0; i < 9; ++i) {
+		
+		if (a == g[y][i]) {
+			return false;
+			
+		}
+		if (a == g[i][x])
+			return false;
+	}
+	return true;
+}
+
+bool back(int idx) {
+	//draw();
+	if (idx >= zeros.size()) return true;
+	int y = zeros[idx].first;
+	int x = zeros[idx].second;
+	
+	for (int i = 1; i < 10; ++i) {
+		if (check(i, y, x)) {
+			g[y][x] = i;
+			if (back(idx + 1)) {
+				return true;
+			}
+			g[y][x] = 0;
+		}
+	}
+	g[y][x] = 0;
+	return false;
+}
+
+int main() {
+	
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			cin >> g[i][j];
+			if (g[i][j] == 0) {
+				zeros.push_back({ i,j });
+			}
+		}
+	}
+
+	back(0);
+
+
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			cout << g[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
