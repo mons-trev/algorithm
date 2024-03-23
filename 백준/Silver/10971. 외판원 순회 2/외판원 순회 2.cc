b60544566453afe00d78591ad36e14a4f96ef7e4
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-03-23 21:58:57 +0900

    [Silver II] Title: 외판원 순회 2, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
@@ -1,24 +1,30 @@
 #include<iostream>
-
+#include<vector>
+#include<algorithm>
+#include<queue>
 
 using namespace std;
 
+vector<pair<int, int>> v;
+
 int n;
 int g[10][10];
-bool v[10];
 
-int sol(int x, int sum, int cnt) {
-	if (cnt == n) {
+bool visit[10];
+
+int back(int x, int cnt) {
+	if (cnt >= n) {
 		if (g[x][0] != 0) {
-			return sum + g[x][0];
+			return g[x][0];
 		}
+		else return 100000000;
 	}
-	int ret = 1000000000;
+	int ret = 100000000;
 	for (int i = 0; i < n; ++i) {
-		if (g[x][i] != 0 && !v[i]) {
-			v[i] = true;
-			ret= min(ret, sol(i, sum + g[x][i], cnt + 1));
-			v[i] = false;
+		if (!visit[i] && g[x][i]!=0) {
+			visit[i] = true;
+			ret = min(ret, g[x][i] + back(i, cnt + 1));
+			visit[i] = false;
 		}
 	}
 	return ret;
@@ -31,6 +37,8 @@ int main() {
 			cin >> g[i][j];
 		}
 	}
-	v[0] = true; // 0번에서 무조건 시작
-	cout << sol(0, 0, 1);
+	
+	visit[0] = true;
+	cout << back(0, 1);
+
 }
