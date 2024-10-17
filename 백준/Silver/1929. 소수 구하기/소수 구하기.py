997d068b3dd0215cf5f5a32861bdd294d6b03fad
Jieun <80975932+mons-trev@users.noreply.github.com>
2024-10-18 02:39:47 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 39368 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,13 @@
+f, e = map(int, input().split())
+era = [0 for i in range(0, e+1)]
+def find(f,e) :
+
+    for i in range(2,e+1) :
+        if(era[i]==1) : 
+            continue
+        if i>=f :
+            print(i)
+        for j in range(i*2, e+1, i) :
+            era[j]=1
+            
+find(f,e)
